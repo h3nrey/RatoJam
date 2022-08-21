53485f4e892eaa61d12a0f7c923d2d9f4195de5d
Davi Aleixo <dac2@cesar.school>
2022-08-21 19:26:42 -0300

    Aleixo-21/08: AaAAAAaAaAaAaAaAaaaaaaaaaaaaaaaaa

--- RatFitti/scenes/game.gd
+++ RatFitti/scenes/game.gd
@@ -2,10 +2,21 @@ extends Node2D
 
 onready var pause = $Pause
 
-func _ready():
-	pass # Replace with function body.
+onready var cronometro = $Cronometro
+var segundos = 00
+var minuto = 00
+var hora = 00
 
-
+func _update_time():
+	if segundos+1 == 60:
+		if minuto+1 == 60:
+			hora +=1
+			minuto = 0
+		minuto += 1
+		segundos = 0
+	segundos += 1
+	
+	cronometro = "Tempo: " + str(hora) + ":" + str(minuto) + ":" + str(segundos)
 
 func _on_TextureButton_pressed():
 	pause._handle_pause()
